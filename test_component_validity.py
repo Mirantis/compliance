10ad7e3eac24dcbc34bf84ab0a8feba0f399a548
Andrew Weiss <anweiss@docker.com>
2016-11-17 16:35:25 -0500

    change schema load path

--- test_component_validity.py
+++ test_component_validity.py
@@ -7,11 +7,13 @@ import yaml
 logging.basicConfig()
 
 def get_schema(version):
-    path = 'schemas/kwalify/component/v{}.yaml'.format(version)
+    """ Load contents of schema file """
+    path = 'v{}.yaml'.format(version)
     contents = open(path)
     return yaml.load(contents)
 
 def create_validator(source_data):
+    """ Generate validator from PyKwalify """
     version = source_data.get('schema_version', '3.1.0')
     schema = get_schema(version)
     validator = Core(source_data={}, schema_data=schema)
@@ -19,7 +21,7 @@ def create_validator(source_data):
     return validator
 
 def test_component_validity():
-    """ Check that the content of data fits with masonry schema v2 """
+    """ Test component validity against the OpenControl schema """
     for component_file in iglob('*/component.yaml'):
         print(component_file)
         source_data = yaml.load(open(component_file))
