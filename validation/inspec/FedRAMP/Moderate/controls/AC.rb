1963c5221c5b7b1873e326effcfa6c75092e3acd
Andrew Weiss <anweiss@docker.com>
2017-08-18 15:41:18 -0400

    ac2-1

--- validation/inspec/FedRAMP/Moderate/controls/AC.rb
+++ validation/inspec/FedRAMP/Moderate/controls/AC.rb
@@ -24,12 +24,13 @@ control 'ac-2' do
   end
 end
 
-control 'ac-2' do
+control 'ac-2 (1)' do
   impact 0.8
   title 'Ensure LDAP integration has been enabled'
   desc '
-    AC-2 Account Management: LDAP should be enabled and configured to help the
-    organization meet the requirements of this control
+    AC-2 (1) Account Management | Automated System Account Management: LDAP
+    should be enabled and configured to help the organization meet the
+    requirements of this control
   '
   describe ucp(val_ucp_uri, val_username, val_password) do
     it { should be_ldap_enabled }
