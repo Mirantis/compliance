90692a742b2a3720eff512bb3fea7bbb2bd4c22a
Andrew Weiss <anweiss@docker.com>
2017-08-21 16:57:55 -0400

    renamed inspec control

--- validation/inspec/FedRAMP/Moderate/controls/ucp.rb
+++ validation/inspec/FedRAMP/Moderate/controls/ucp.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # copyright: 2017, Docker, Inc
 
-title 'Access Control (AC)'
+title 'Universal Control Plane'
 
 val_ucp_uri = attribute('ucpuri', description: 'UCP URI')
 val_username = attribute('username', description: 'UCP username')
